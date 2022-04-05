07aea019b7fc8c7c6d30892807d845886dc9d637
”variot” <variot.anthony@gmail.com>
2022-04-05 12:27:03 +0200

    RPG: Warrior

---
+++ src/org/humanbooster/bank/model/rpg/Warrior.java
@@ -0,0 +1,22 @@
+package org.humanbooster.bank.model.rpg;
+
+public class Warrior extends Humanoid{
+    private boolean doubleAttackReady = true;
+
+    public Warrior(String name, double health, int force) {
+        super(name, health, force);
+    }
+
+    public void doubleAttack(Humanoid h){
+        h.receiveDamage(this.force*0.7);
+        h.receiveDamage(this.force*0.7);
+        this.doubleAttackReady=false;
+    }
+
+    @Override
+    public void attack(Humanoid h){
+        super.attack(h);
+        this.doubleAttackReady = true;
+    }
+
+}
